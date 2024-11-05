4b5449c62af99aa7c518d84e33fb26dfa48866b8
bryan <>
2024-11-05 13:46:48 +0330

    apis done

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,40 @@ import config from "./config";
 
 const PORT = config.port;
 
+// Connect to the database and start the server
+const connectWithRetry = async (retries: number = 5, delay: number = 2000) => {
+    while (retries) {
+        try {
+            // Check database connection
+            await pool.getConnection();
+            console.log("Successfully connected to the database.");
+
+            // Start the server only if the database connection is successful
+            app.listen(PORT, () => {
+                console.log(`Server running on port ${PORT}`);
+            });
+            break;
+        } catch (error) {
+            console.error(`Database connection failed: ${error}`);
+            retries -= 1;
+            console.log(`Retries left: ${retries}`);
+
+            // Exit if no retries left
+            if (retries === 0) {
+                console.error("Could not connect to the database. Exiting...");
+                process.exit(1);
+            }
+
+            // Wait for the specified delay before the next retry
+            await new Promise((resolve) => setTimeout(resolve, delay));
+        }
+    }
+};
+
 const startServer = async () => {
     try {
         // Check database connection
-        await pool.getConnection();
+        await connectWithRetry();
         console.log("Successfully connected to the database.");
 
         // Start the server only if the database connection is successful
